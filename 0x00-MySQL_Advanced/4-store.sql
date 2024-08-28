4341c76af75bfa6220ed8e0a23da152e99a857a1
Ramyrashad127 <ramy41599@gmail.com>
2024-08-28 16:50:10 +0300

    new

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,4 +1,4 @@
 -- crate trigger
-CREATE trigger AFTER INSERT ON orders
+CREATE trigger decrease_items AFTER INSERT ON orders
 FOR EACH ROW
 UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
