cfee7d2ddd1d3b21891608cca884ae41d6985d1c
Ramyrashad127 <ramy41599@gmail.com>
2024-09-11 18:58:49 +0300

    new

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -12,18 +12,18 @@ redis_store = redis.Redis()
 
 
 def data_cacher(method: Callable) -> Callable:
-    """ data docrator"""
+    """ data"""
     @wraps(method)
     def invoker(url) -> str:
-        """ wapped function"""
+        """ wrapped"""
         redis_store.incr(f'count:{url}')
-        res = redis_store.get(f'result:{url}')
-        if res:
-            return res.decode('utf-8')
-        res = method(url)
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
         redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, res)
-        return res
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return invoker
 
 
