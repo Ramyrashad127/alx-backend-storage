fb5a85465e9ecca2b08dcf95e35d0b6aee56b86d
Ramyrashad127 <ramy41599@gmail.com>
2024-09-11 18:01:50 +0300

    new

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache():
@@ -18,3 +18,20 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], any]] = None) -> Optional[any]:
+        """base get method"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """convert o utf-8"""
+        return self.get(key, lambda x: x.decode('utf-8') if x is not None else None)
+
+    def get_int(self, key: str) -> Optional[int]:
+        """convert to int"""
+        return self.get(key, lambda x: int(x) if x is not None else None)
