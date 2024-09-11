2a5f4cc35d6c822bc1beadeab5f99144159f4fde
Ramyrashad127 <ramy41599@gmail.com>
2024-09-11 18:57:49 +0300

    new

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+'''A module with tools for request caching and tracking.
+'''
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+"""intialize the redis instance."""
+
+
+def data_cacher(method: Callable) -> Callable:
+    """ data docrator"""
+    @wraps(method)
+    def invoker(url) -> str:
+        """ wapped function"""
+        redis_store.incr(f'count:{url}')
+        res = redis_store.get(f'result:{url}')
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, res)
+        return res
+    return invoker
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    """ retrive data"""
+    return requests.get(url).text
