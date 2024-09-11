23d1278a6e965c722001f2f289c5afccc73b74cb
Ramyrashad127 <ramy41599@gmail.com>
2024-09-11 16:26:07 +0300

    new

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ 
-no modules
+pymongo
 """
 
 
@@ -14,13 +14,13 @@ def log_stats():
     client = MongoClient('mongodb://127.0.0.1:27017')
     logs_collection = client.logs.nginx
     total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
     post = logs_collection.count_documents({"method": "POST"})
     put = logs_collection.count_documents({"method": "PUT"})
     patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
     path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
+    get = logs_collection.count_documents({"method": "GET"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
     print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
